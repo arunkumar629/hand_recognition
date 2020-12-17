1c326e55d388a318ba092cd7d2e0adc089ee029c
arun <arunkumar629@gmail.com>
2020-12-17 15:06:16 +0530

    csv_creator_added

---
+++ csv_creator.py
@@ -0,0 +1,10 @@
+import os
+for name in ['testing']:
+    with open('hand_dataset_{}.csv'.format(name), 'w') as output_file:
+        print('=== creating {} dataset ==='.format(name))
+        output_file.write('image_path,label\n')
+        for i in range(4):
+            path = '{}\{}'.format(name, i)
+            for file in os.listdir(path):
+                if file.endswith(".jpg"):
+                    output_file.write('{},{}\n'.format(os.path.join(path, file), str(i)))
